6ef84f6833fe6709d631bf26fdf1644e2706fa12
Anna Yemelyanova <devildriver2310@gmail.com>
2022-11-03 17:47:46 +0300

    refactor: provide arguments to function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,14 +4,16 @@ const path = require('path');
 const filesPath = path.join(__dirname, 'styles');
 const newFilePath = path.join(__dirname, 'project-dist');
 
-const createBundle = async () => {
-  const files = await fs.readdir(filesPath, { withFileTypes: true })
+const createBundle = async (currentPath, resultPath, fileName) => {
+  const files = await fs.readdir(currentPath, { withFileTypes: true })
   const filtredFiles = files.filter(item => path.extname(item.name) === '.css' && item.isFile());
 
-  const filesContent = await Promise.all(filtredFiles.map(file => fs.readFile(path.join(filesPath, file.name), 'utf-8')));
+  const filesContent = await Promise.all(filtredFiles.map(file => fs.readFile(path.join(currentPath, file.name), 'utf-8')));
   
-  await fs.writeFile(path.join(newFilePath, 'bundle.css'), filesContent.join('\n'));
+  await fs.writeFile(path.join(resultPath, fileName), filesContent.join('\n'));
   
 }
 
-createBundle();
+createBundle(filesPath, newFilePath, 'bundle.css');
+
+exports.createBundle = createBundle;
